d5783a749a326271ea93cc3590069f7237d7e958
“Mrinmoyee <mrmishra@ucsc.edu>
2023-08-14 10:01:13 -0700

    finished audio button component

--- src/components/audioButton.js
+++ src/components/audioButton.js
@@ -1,13 +1,42 @@
-import React, {useRef} from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 
 const AudioButton = ({trackName, trackArtists, previewAudioUrl, externalUrl}) => {
-    console.log("inside audio button", trackName, trackArtists, previewAudioUrl, externalUrl)
+    const [isPlaying, setIsPlaying] = useState(false)
+    const [audioInstance, setAudioInstance] = useState(null)
+
+    const handleAudioClick = () => {
+        if (audioInstance) {
+            audioInstance.pause()
+            setAudioInstance(null)
+        }
+
+        const newAudioInstance = new Audio(previewAudioUrl)
+        if (isPlaying) {
+            newAudioInstance.pause();
+        } else {
+            newAudioInstance.play();
+        }
+        setIsPlaying(!isPlaying);
+        setAudioInstance(newAudioInstance)
+
+    }
+
+    useEffect(() => {
+        return () => {
+
+            if (audioInstance) {
+                audioInstance.pause()
+                setAudioInstance(null)
+            }
+        }
+    }, [audioInstance]);
+
     return (
     <div>
-        <button className='trackBtn'>
-            {trackName} - {trackArtists}
+        <button className='trackBtn' onClick={handleAudioClick}>
+            {isPlaying ? "Pause" : "Play"}: {trackName} - {trackArtists}
         </button>
-
+        <a href={externalUrl} style={{color: "#FFDDE2"}} target="_blank" rel="noreferrer">Open with Spotify</a>
     </div>
     )
     
